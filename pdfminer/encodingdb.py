8518baccc3fa75c39ad569d3d09e9ac5df7f1d78
xujb <janbox@sina.com>
2019-05-28 18:32:38 +0800

    add align-name for some glyphname

--- pdfminer/encodingdb.py
+++ pdfminer/encodingdb.py
@@ -8,12 +8,43 @@ from .latin_enc import ENCODING
 STRIP_NAME = re.compile(r'[0-9]+')
 
 
+# windings ...
+glyphname2unicode_ext = {
+    'oneinv': u'\u278A',
+    'twoinv': u'\u278B',
+    'threeinv': u'\u278C',
+    'fourinv': u'\u278D',
+    'fiveinv': u'\u278E',
+    'sixinv': u'\u278F',
+    'seveninv': u'\u2790',
+    'eightinv': u'\u2791',
+    'nineinv': u'\u2792',
+    'teninv': u'\u2793',
+
+    'onesansinv': u'\u278A',
+    'twosansinv': u'\u278B',
+    'threesansinv': u'\u278C',
+    'foursansinv': u'\u278D',
+    'fivesansinv': u'\u278E',
+    'sixsansinv': u'\u278F',
+    'sevensansinv': u'\u2790',
+    'eightsansinv': u'\u2791',
+    'ninesansinv': u'\u2792',
+    'tensansinv': u'\u2793',
+
+    'handbckptright': u'\u261E',
+    'xrhombus': u'\u2756',
+    'boxcheckbld': u'\u2611',
+}
+
 ##  name2unicode
 ##
 def name2unicode(name):
     """Converts Adobe glyph names to Unicode numbers."""
     if name in glyphname2unicode:
         return glyphname2unicode[name]
+    if name in glyphname2unicode_ext:
+        return glyphname2unicode_ext[name]
     m = STRIP_NAME.search(name)
     if not m:
         raise KeyError(name)
